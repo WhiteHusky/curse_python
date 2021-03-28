2ed855c302328c4271fb7c937b29ca2b6676595b
Carlen White <WhiterSuburban@gmail.com>
2021-03-28 05:10:47 -0400

    Nomenclature change
    Using Addon instead of Mod

--- curse_python/cli/__init__.py
+++ curse_python/cli/__init__.py
@@ -11,11 +11,11 @@ import sys
 from os import remove
 from pathlib import Path
 
-parser = argparse.ArgumentParser(description='Download Minecraft Mods')
+parser = argparse.ArgumentParser(description='Download CurseForge Addons')
 parser.add_argument('addon_ids', metavar='a', type=int, nargs='?', help='Addons to download')
 parser.add_argument('--target', type=str, nargs='+', help='Acceptable version targets')
-parser.add_argument('--save', required=True, type=str, help='Save location for mods')
-parser.add_argument('--manifest', default='./mod-manifest.json', type=str, help='Manifest location for updating mods')
+parser.add_argument('--save', required=True, type=str, help='Save location for Addons')
+parser.add_argument('--manifest', default='./addon-manifest.json', type=str, help='Manifest location for updating addons')
 
 def download_addon(save_location, addon_file):
     r = requests.get(addon_file.download_url, stream=True)
