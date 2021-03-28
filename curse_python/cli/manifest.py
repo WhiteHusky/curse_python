5c853cad75205ebfb0ea5ec0fc9840d22cd719e4
Carlen White <WhiterSuburban@gmail.com>
2021-03-28 04:43:48 -0400

    Made initialization sane
    Instead of ModManifest init handling the initial data, it'll be done by
    the fromjson function

--- curse_python/cli/manifest.py
+++ curse_python/cli/manifest.py
@@ -6,30 +6,19 @@ from curse_python.addons import AddonFile
 
 class DownloadedAddon(object):
     def __init__(self, addon_id, filename, date_time):
-        self.addon_id = addon_id
-        self.file_name = filename
-        self.file_date = datetime.fromisoformat(date_time) if isinstance(date_time, str) else date_time
+        self.addon_id: int = addon_id
+        self.file_name: str = filename
+        self.file_date: datetime = date_time
 
 class ModManifest(object):
     """
     Object for mananging a mod manifest file
     Handles serialization
     """
-    def __init__(self, version_targets, wanted_addons, addon_ids_to_file_name_and_iso_format):
-        afi = addon_ids_to_file_name_and_iso_format
+    def __init__(self):
         self.version_targets: List[str] = []
         self.wanted_addons: List[int] = []
         self.downloaded_addons: List[DownloadedAddon] = []
-        for v in version_targets:
-            self.version_targets.append(v)
-
-        for a in wanted_addons:
-            self.wanted_addons.append(a)
-
-        for addon_id in addon_ids_to_file_name_and_iso_format:
-            self.downloaded_addons.append(
-                DownloadedAddon(int(addon_id), afi[addon_id]['file_name'], afi[addon_id]['iso_datetime'])
-            )
     
     def downloaded_addon(self, addon_file: AddonFile):
         self.downloaded_addons.append(
@@ -39,16 +28,28 @@ class ModManifest(object):
                 addon_file.file_date
             )
         )
-
-
-    @staticmethod
-    def empty():
-        return ModManifest([],[],[])
     
     @staticmethod
     def fromjson(jsonstring):
         json_object = json.loads(jsonstring)
-        return ModManifest(json_object["version_targets"], json_object["wanted_addons"], json_object["downloaded_addons"])
+        mod_manifest = ModManifest()
+        for version_target in json_object["version_targets"]:
+            mod_manifest.version_targets.append(version_target)
+
+        for wanted_addons in json_object["wanted_addons"]:
+            mod_manifest.wanted_addons.append(int(wanted_addons))
+
+        d = json_object["downloaded_addons"]
+
+        for addon_id in d:
+            mod_manifest.downloaded_addons.append(
+                DownloadedAddon(
+                    int(addon_id),
+                    d[addon_id]['file_name'],
+                    datetime.fromisoformat(d[addon_id]['iso_datetime'])
+                )
+            )
+        return mod_manifest
     
     def tojson(self, *args, **kwargs):
         json_object = {
